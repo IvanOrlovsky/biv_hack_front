6737fc04d19c61f197a04c48cca780f0f4c59c8c
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-11-16 22:18:08 +0300

    через use достаю id

--- src/app/product/[id]/info/page.tsx
+++ src/app/product/[id]/info/page.tsx
@@ -1,9 +1,14 @@
+import React from "react";
+
 export type InfoPageProps = {
-	params: {
+	params: Promise<{
 		id: string;
-	};
+	}>;
 };
 
-export default function Info({ params: { id } }: InfoPageProps) {
+export default function Info(props: InfoPageProps) {
+	const params = React.use(props.params);
+	const { id } = params;
+
 	return <main>{id}</main>;
 }
